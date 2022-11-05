6e0980afcae45da39fc9d14d8685a5ae1caec9e1
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2022-11-06 01:34:38 +0900

    Initial commit

---
+++ src/ch02/sec13/ScannerExample.java
@@ -0,0 +1,38 @@
+package ch02.sec13;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("x의 값 입력 > ");
+        String strX = scanner.nextLine();
+        int x = Integer.parseInt(strX);
+
+        System.out.println("y의 값 입력 > ");
+        String strY = scanner.nextLine();
+        int y = Integer.parseInt(strY);
+
+        int result = x + y;
+        System.out.println("[결과] x + y: " + result);
+        System.out.println();
+
+        while(true) {
+            System.out.print("입력 문자열: ");
+            String data = scanner.nextLine();
+
+            if(data.equals("quit()")) {
+                break;
+            }
+            System.out.println("출력 문자열: " + data);
+            System.out.println();
+        }
+
+        System.out.println("[ 종료 ]");
+
+    }
+
+}
