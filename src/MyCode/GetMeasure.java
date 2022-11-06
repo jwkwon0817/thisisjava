0dc6369434d6eeca9c3ae5fec22c76aa4fd15bd1
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2022-11-06 11:09:14 +0900

    Get Measures of Number

---
+++ src/MyCode/GetMeasure.java
@@ -0,0 +1,25 @@
+package MyCode;
+
+import java.util.Scanner;
+
+public class GetMeasure {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("숫자 입력 >>> ");
+        String strN = scanner.nextLine();
+        int n = Integer.parseInt(strN);
+
+        System.out.println(n + "의 약수들: ");
+
+        for( int i=1; i<=n; i++ ) {
+            if( n%i==0 ) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
