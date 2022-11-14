4a414465c957f902cffbb1c9b51afeb1ca94e0a6
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2022-11-14 23:43:30 +0900

    Initial commit

--- src/mycode/test.java
+++ src/mycode/test.java
@@ -1,26 +1,5 @@
 package mycode;
 
-public class test {
-    public static void main(String[] args) {
-        int[][] arr = {
-                { 95, 86 },
-                { 83, 92, 96 },
-                { 78, 83, 93, 87, 88 }
-        };
+public class test{
 
-        double sum = 0;
-        double avg = 0;
-        int count = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                count += 1;
-                sum += arr[i][j];
-            }
-        }
-
-        avg = sum / count;
-
-        System.out.println(avg);
-    }
 }
